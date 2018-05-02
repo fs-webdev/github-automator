3abce5155dc23c4d86431722649c3c900749d1f3
Joey Cozza <joeycozza@gmail.com>
2018-05-02 15:57:09 -0600

    allowing for much larger payloads

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const errorHandler = require('errorhandler');
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.use(morgan('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json({limit: '10mb'}));
+app.use(bodyParser.urlencoded({limit: '10mb', extended: true}));
 
 if (app.get('env') === 'development') {
   app.use(errorHandler());
